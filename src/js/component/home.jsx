141090471afac89bf0b634316d7ece18d9e77d07
Carmen Lopez <132434208+clopezva@users.noreply.github.com>
2023-05-26 12:27:20 +0000

    finish code

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,29 @@
 import React from "react";
+import useSate  from 'react'
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	
+	const [color, setColor] = useSate('red')
+
+	function handleClick(event){
+		//console.log(event.target.id)
+		//setColor(event.target.id)
+		setColor(event.target.id)
+		console.log(color)
+	}
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<div className="traffic-light">
+				<span onClick={handleClick} id="red" className={`circle red-light `}></span>
+				<span onClick={handleClick} id="yellow "className={`circle yellow-light`}></span>
+				<span onClick={handleClick} id="green" className={`circle green-light`}></span>
+			</div>
 		</div>
 	);
 };
